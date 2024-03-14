a965c7ca3a2d7b4379ba31fc21bd22013fef965d
NearFactory <93475805+nearfactory@users.noreply.github.com>
2024-03-14 22:12:19 +0900

    Add files via upload

--- asset/js/script_swipe.js
+++ asset/js/script_swipe.js
@@ -46,17 +46,3 @@
 //     }
 //   }
 // })
-
-
-document.querySelectorAll('.left').forEach(elm => {
-	elm.onclick = function () {
-		let ul = elm.parentNode.querySelector('ul');
-		ul.scrollLeft -= ul.clientWidth;
-	};
-});
-document.querySelectorAll('.right').forEach(elm => {
-	elm.onclick = function () {
-		let ul = elm.parentNode.querySelector('ul');
-		ul.scrollLeft += ul.clientWidth;
-	};
-});
